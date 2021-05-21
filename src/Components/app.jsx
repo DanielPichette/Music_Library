9ae2e82852d326cb8ee5c390e102a5be7f86dc67
DanielPichette <daniel.f.pichette@gmail.com>
2021-05-20 20:03:15 -0500

    updated the initial class app.jsx file. within the constructor

--- src/Components/app.jsx
+++ src/Components/app.jsx
@@ -2,19 +2,24 @@ import axios from 'axios';
 import React, { Component } from 'react';
 
 class App extends Component {
-    constructor(){
-        super();
+    constructor(props) {
+        super(props);
+        this.songs = [];
+
+        this.state = {
+            songNumber: 0
+        }
     }
-    state = { 
-        songLibrary: []
-     }
+
 componentDidMount(){
     axios.get('http://127.0.0.1:8000/music/')
     .then(response=>{
-        console.log(response.data);
+        //ßconsole.log(response.data);
         this.setState({
             songLibrary:response.data
         })
+        this.songs = response.data
+        console.log(this.songs, 'songs')
     })
 }
 
@@ -22,7 +27,7 @@ componentDidMount(){
 
 
     render() { 
-        console.log(this.state.songLibrary)
+        //console.log(this.state.songLibrary)
         return ( 
             <h1>Our React App Using a Component</h1>
          );

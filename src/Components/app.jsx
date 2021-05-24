7f3f41aece6f19f7ace484a3b5ac14405fc8d5f0
DanielPichette <daniel.f.pichette@gmail.com>
2021-05-24 16:25:27 -0500

    added search functionality within the main app.jscx component within th e onChange Function

--- src/Components/app.jsx
+++ src/Components/app.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import TitleBar from './TileBar/titleBar';
 import SongListTable from './Table/Table';
 import AddSong from './SongCreationForm/songCreationForm';
-import Searchbar from './SearchBar/searchbar';
 
 class App extends Component {
     constructor(props) {
@@ -16,21 +15,21 @@ class App extends Component {
                 {id: '2', title: "Pretty Please", artist: "Dua Lipa", album: "Future Nostalgia", genre: 'pop',release_date: "2021-03-27"},
                 {id: '3', title: "Fake Mona Lisa", artist: "Carly Rae Jepson", album: "Dedicated Side B", genre: 'pop',release_date: "2020-05-21"},
                 {id: '4', title: "Ocean Drive", artist: "Duke Dumont", album: "Duality", genre: 'indie pop',release_date: "2015-07-21"},
-                {id: '6', title: "Good 4 you", artist: "Olivia Rodrigo", album: "Good 4 you", genre: 'pop',release_date: "2021-05-11T"},
+                {id: '6', title: "Good 4 you", artist: "Olivia Rodrigo", album: "Good 4 you", genre: 'pop',release_date: "2021-05-11"},
                 ],
-            searchInput: ""
+            searchInput: "",
+            filteredSearch: [],
         }
     }
 
     componentDidMount(){
         axios.get('http://127.0.0.1:8000/music/')
         .then(response=>{
-            console.log('has mounted')
+            //console.log('has mounted')
             //console.log(response.data);
             //this.setState({
-            //songLibrary:response.data
+            //songs :response.data
             // })
-            //this.songs = response.data
             console.log(this.state.songs, 'songs')
 
         });
@@ -43,17 +42,56 @@ class App extends Component {
         console.log(this.state.songs, 'updated with new song')
     }
 
-    onChange = event =>{
+    filtered(){
+        //let searchInput = this.state.searchInput
+        let allSongs = this.state.songlist
+        let filteredSongs = allSongs.filter(song => 
+            song.title.includes(this.state.searchInput) ||
+            song.artist.includes(this.state.searchInput) ||
+            song.album.includes(this.state.searchInput) ||
+            song.genre.includes(this.state.searchInput) ||
+            song.release_date.includes(this.state.searchInput));
         this.setState({
-            searchInput: event.target.value
-        });
-        console.log(this.state.searchInput)
+            filteredSearch: filteredSongs
+        })
+        console.log(this.state.filteredSearch)
     }
 
 
 
+    onChange = event =>{
+        let allSongs = this.state.songs
+        this.setState({
+            searchInput: event.target.value,
+        });
+        let filteredSongs = this.state.songs.filter(song => 
+            song.title.includes(this.state.searchInput) ||
+            song.artist.includes(this.state.searchInput) ||
+            song.album.includes(this.state.searchInput) ||
+            song.genre.includes(this.state.searchInput) ||
+            song.release_date.includes(this.state.searchInput));
+        this.setState({
+            filteredSearch: filteredSongs,
+        });
+        if(this.state.searchInput === ""){
+            this.setState({
+                filteredSearch : allSongs
+            });
+            
+
+        }
+        console.log(this.state.searchInput)
+        console.log(this.state.filteredSearch)
+    }
+
+
+
+    
+
     render() { 
         //console.log(this.songs[1].id)
+        
+        
         return ( 
         <div className="App">
             <TitleBar />
@@ -61,11 +99,14 @@ class App extends Component {
             <input type="text" name="searchbar" placeholder="Search..."  onChange={this.onChange}></input>
             </div>
             <div>
-                <SongListTable songlist={this.state.songs}  song={this.state.songs[this.state.songNumber]}/>
+                <SongListTable key="key" songlist={this.state.filteredSearch}  song={this.state.songs[this.state.songNumber]} searchInput={this.state.searchInput}/>
             </div>
             <div>
                 <h2>Add Song</h2>
                 <AddSong addSong={(thesong) => this.addSong(thesong)}/>
+            </div>
+            <div>
+                {this.state.searchInput}
             </div>
         </div>
          );
